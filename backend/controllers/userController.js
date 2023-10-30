15d169786b8268397ecb5d13ea7efc8b23e5f3d9
Ying Quan <quanying26@hotmail.com>
2023-10-30 10:26:24 -0400

    User password an d email validation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,89 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/UserModel.js";
+//  Auth user & get token
+// route POST /api/users/login
+//public
+const authUser = asyncHandler(async (req, res) => {
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email});
+  if(user && (await user.matchPassword(password))){
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+//  Register User
+// route POST /api/users
+//public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
+});
+
+//  Logout user & clear cokkie
+// route POST /api/users/logout
+//private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
+});
+
+//  get User profile
+// route GET /api/users/profile
+// private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
+});
+
+//  get User profile
+// route PUT /api/users/profile
+// private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update user profile");
+});
+
+//  get Users
+// route GET /api/users
+// private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get user by id");
+});
+
+//  get User by ID
+// route GET /api/users/:id
+// private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
+});
+
+// Delete Users
+// route DELETE /api/users/:id
+// private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete user");
+});
+
+//  Update User
+// route PUT /api/users/:ID
+// private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUsers,
+  getUserById,
+  getUserProfile,
+  updateUserProfile,
+  updateUser,
+  deleteUser,
+} 
