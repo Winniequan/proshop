f9e60b6cf656b5a76b9f5767ae76f9191c8525d8
Ying Quan <quanying26@hotmail.com>
2023-10-30 22:51:26 -0400

    userRoutes backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js"
+
 import generateToken from "../utils/generateToken.js";
 //  Auth user & get token
 // route POST /api/users/login
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,6 +11,8 @@ const port = process.env.PORT || 5000;
 connectDB(); // connect to MongoDB
 
 const app = express();
+
+cors({ credentials: true, origin: true });
 
 // Body Parser  middleware
 app.use(express.json());
